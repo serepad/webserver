75e09d989ca5d6c75ab6e8321c4f1b7fe2d5e6ab
Sergey Padzhev <serhio_j@mail.ru>
2017-07-10 20:34:32 +0300

    Chat services modified.

--- src/main/java/chat/ChatWebSocket.java
+++ src/main/java/chat/ChatWebSocket.java
@@ -1,7 +1,47 @@
 package chat;
 
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
 /**
  * Created by SERhiO on 10.07.2017.
  */
+
+@SuppressWarnings("UnusedDeclaration")
+@WebSocket
 public class ChatWebSocket {
+    private chat.ChatService chatService;
+    private Session session;
+
+    public ChatWebSocket(ChatService chatService) {
+        this.chatService = chatService;
+    }
+
+    @OnWebSocketConnect
+    public void onOpen(Session session) {
+        chatService.add(this);
+        this.session = session;
+    }
+
+    @OnWebSocketMessage
+    public void onMessage(String data) {
+        chatService.sendMessage(data);
+    }
+
+    @OnWebSocketClose
+    public void onClose(int statusCode, String reason) {
+        chatService.remove(this);
+    }
+
+    public void sendString(String data) {
+        try {
+            session.getRemote().sendString(data);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
+
