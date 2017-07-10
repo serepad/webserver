1a860ddd01fee4feeb0a6c37ba8ead355c83853a
Sergey Padzhev <serhio_j@mail.ru>
2017-07-10 20:34:55 +0300

    Chat service modified.

--- src/main/java/chat/ChatService.java
+++ src/main/java/chat/ChatService.java
@@ -1,7 +1,34 @@
 package chat;
 
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * Created by SERhiO on 10.07.2017.
  */
 public class ChatService {
+    private Set<ChatWebSocket> webSockets;
+
+    public ChatService() {
+        this.webSockets = Collections.newSetFromMap(new ConcurrentHashMap<>());
+    }
+
+    public void sendMessage(String data) {
+        for (ChatWebSocket user : webSockets) {
+            try {
+                user.sendString(data);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public void add(ChatWebSocket webSocket) {
+        webSockets.add(webSocket);
+    }
+
+    public void remove(ChatWebSocket webSocket) {
+        webSockets.remove(webSocket);
+    }
 }
